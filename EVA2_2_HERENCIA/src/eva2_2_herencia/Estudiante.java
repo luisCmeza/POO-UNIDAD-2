d2c173c6a18481820331a6a4d313020a08b5ba99
LuisC <124108074+luisCmeza@users.noreply.github.com>
2023-03-12 18:56:44 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Estudiante.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_2_herencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String noControl;
+
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(String noControl) {
+        this.noControl = noControl;
+    }
+
+    
+    
+}
